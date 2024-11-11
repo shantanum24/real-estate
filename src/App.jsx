fd788ae98b004a7ffb412ce9d4ea6ab66914a935
Shantanu Mukkawar <shantanum@Shantanus-MacBook-Air.local>
2024-11-11 15:58:55 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+         <h1 className='text-center'>hello</h1>
+         <h1>
+          bye
+         </h1>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
